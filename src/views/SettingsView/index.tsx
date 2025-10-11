4d67d810670f5b3a66f3f193b7073fc93b131856
lzy <yosora2016@gmail.com>
2025-10-11 18:16:23 +0800

    feat: 自动检查app最新版本, 手动选择更新

--- src/views/SettingsView/index.tsx
+++ src/views/SettingsView/index.tsx
@@ -1,24 +1,70 @@
 import {
+  closeApp,
   deleteKVStorage,
+  getAppInfo,
   getKVStorage,
   setKVStorage,
   showAlert,
   showHUD,
+  updateCurrentApp,
   vibrate,
 } from "minip-bridge";
 import { apiConfig } from "../../api/api";
 import ArrowRight from "../../assets/arrow-right.svg";
 import "./index.css";
-import { createSignal, onMount } from "solid-js";
+import { createSignal, onMount, Show } from "solid-js";
+
+const CHECK_VERSION_URL = "https://api.github.com/repos/MikazukiY/pica-minip/releases/latest"
+
+function compareVersions(version1: string, version2: string): number {
+  const v1 = version1.split('.').map(Number);
+  const v2 = version2.split('.').map(Number);
+
+  const maxLength = Math.max(v1.length, v2.length);
+
+  for (let i = 0; i < maxLength; i++) {
+    const num1 = v1[i] || 0;
+    const num2 = v2[i] || 0;
+
+    if (num1 > num2) return 1;
+    if (num1 < num2) return -1;
+  }
+
+  return 0;
+}
 
 export default function SettingsView() {
   const [api, setSS] = createSignal("2");
+  const [version, setVersion] = createSignal("")
+  const [latestVersion, setLatestVersion] = createSignal("")
+  const [loading, setLoading] = createSignal(true)
   onMount(() => {
     getKVStorage("selectedApi").then((res) => {
       if (res.data) setSS(res.data);
       else setSS(apiConfig.selected.toString());
     });
+    getAppInfo().then(r => setVersion(r.data.version ?? "unknown"))
+      .finally(() => {
+        fetch(CHECK_VERSION_URL)
+          .then(res => res.json())
+          .then(res => {
+            setLatestVersion(res.tag_name ?? "unknown")
+            console.log(res)
+          })
+          .catch(err => {
+            showHUD({
+              type: "error",
+              title: "检查更新发生错误",
+              message: err?.message ?? "unknown error"
+            })
+          })
+          .finally(() => setLoading(false))
+      })
   });
+
+  function needUpdate() {
+    return compareVersions(version(), latestVersion()) === -1
+  }
 
   function setNew(s: string) {
     setSS(s);
@@ -153,6 +199,52 @@ export default function SettingsView() {
                 {apiConfig.apis[parseInt(api())]}
               </div>
               <img src={ArrowRight} />
+            </div>
+          </div>
+          <hr
+            style={{
+              margin: "0 0 0 12px",
+            }}
+          />
+          <div
+            style={{
+              display: "flex",
+              "justify-content": "space-between",
+              "align-items": "center",
+              padding: "4px 13px",
+              margin: 0,
+              "min-height": "35px",
+            }}
+          >
+            <div>版本</div>
+            <div style={{ color: "gray" }}>
+              {loading() ? "检测中" : version()}
+              <Show when={!loading() && version() !== "unknown" && latestVersion() !== "unknown" && needUpdate()}>
+                &nbsp;(&nbsp;最新版本 {latestVersion()}, <span onClick={() => {
+                  showHUD({
+                    type: "progress",
+                    interaction: false
+                  })
+                  updateCurrentApp("https://github.com/MikazukiY/pica-minip/releases/latest/download/pica-minip.zip")
+                    .then(() => {
+                      showHUD({
+                        type: "success",
+                        message: "更新成功, 即将重启应用",
+                        delay: 1000
+                      })
+                      setTimeout(() => {
+                        closeApp()
+                      }, 1000)
+                    })
+                    .catch(err => {
+                      showHUD({
+                        type: "error",
+                        title: "更新时发生错误",
+                        message: err?.message ?? "unknown error"
+                      })
+                    })
+                }} style={{ color: "red", "font-weight": "bold" }}>点击更新</span>&nbsp;)&nbsp;
+              </Show>
             </div>
           </div>
         </div>
